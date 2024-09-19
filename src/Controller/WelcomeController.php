2c13649b0e9716496dd568d69f5a5c6a8ebd6e27
bfalke <b.falke@bitexpert.de>
2024-09-19 02:22:09 +0200

    Add basic welcome page

---
+++ src/Controller/WelcomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class WelcomeController extends AbstractController
+{
+    #[Route('/')]
+    public function number(): Response
+    {
+        return $this->render('welcome/index.html.twig');
+    }
+}
